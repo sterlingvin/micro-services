c42600d0be3681e64a410e8dad0cb80d211324dd
sterlingvin <135068864+sterlingvin@users.noreply.github.com>
2025-02-19 10:51:02 -0500

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 module "vpc" {
-    source = "./modules/vpc"
+  source = "./modules/vpc"
 
-    vpc_cidr = var.vpc_cidr
-    public_subnet_cidr = var.public_subnet_cidr
-    private_subnet_cidr = var.private_subnet_cidr
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
 }
-  
-module "ecr" {
-    source = "./modules/ecr"
 
-    ecr_repo_name = var.ecr_repo_name
+module "ecr" {
+  source = "./modules/ecr"
+
+  ecr_repo_name = var.ecr_repo_name
 }
 
 module "ecs" {
-    source = "./modules/ecs"
+  source = "./modules/ecs"
 
-    vpc_id = module.vpc.vpc_id
-    public_subnet_ids = module.vpc.public_subnet_ids
-    private_subnet_ids = module.vpc.private_subnet_ids
-    ecr_repo_url = module.ecr.ecr_repo_url
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
+  ecr_repo_url       = module.ecr.ecr_repo_url
 }
 
 module "api_gateway" {
-    source = "./modules/api_gateway"
+  source = "./modules/api_gateway"
 
-    ecs_service_endpoint = module.ecs.ecs_service_endpoint
+  ecs_service_endpoint = module.ecs.ecs_service_endpoint
 
 }
 
--- variables.tf
+++ variables.tf
@@ -1,38 +1,38 @@
 variable "aws_region" {
-    description = "value of the region"
-    type = string
-    default = "ca-central-1"
+  description = "value of the region"
+  type        = string
+  default     = "ca-central-1"
 }
 
 variable "vpc_cidr" {
-    description = "CIDR block for the VPC"
-    type = string
-    default = "10.0.0.0/16"
-  
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+
 }
 
 variable "public_subnet_cidr" {
-    description = "value of the public subnet CIDR block"
-    type = string
-    default = "10.0.1.0/24"
+  description = "value of the public subnet CIDR block"
+  type        = string
+  default     = "10.0.1.0/24"
 }
-  
-variable "private_subnet_cidr"{
-    description = "value of the private subnet CIDR block"
-    type = string
-    default = "10.0.2.0/24"
+
+variable "private_subnet_cidr" {
+  description = "value of the private subnet CIDR block"
+  type        = string
+  default     = "10.0.2.0/24"
 }
-  
-variable "ecr_repo_name"{
-    description = "value of the ECR repository name"
-    type = list(string)
-    default = ["user-service", "product-service", "order-service", "payment-service"]
+
+variable "ecr_repo_name" {
+  description = "value of the ECR repository name"
+  type        = list(string)
+  default     = ["user-service", "product-service", "order-service", "payment-service"]
 }
-  
+
 
 variable "ecr_repo_url" {
-    description = "URL of the ECR repository"
-    type = string
-    default = "851725517396.dkr.ecr.us-east-1.amazonaws.com/user-service"
-  
+  description = "URL of the ECR repository"
+  type        = string
+  default     = "851725517396.dkr.ecr.us-east-1.amazonaws.com/user-service"
+
 }
