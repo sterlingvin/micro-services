12b055c9de29e34e605afb4f9323e6a3af8fa01e
sterlingvin <135068864+sterlingvin@users.noreply.github.com>
2025-02-26 11:50:39 -0500

    working on ecs service

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,6 +1,5 @@
 resource "aws_ecs_cluster" "cluster" {
-    name = "micro-service-cluster"
-
+  name = "micro-service-cluster"
 }
 
 resource "aws_ecs_task_definition" "task" {
@@ -14,10 +13,10 @@ resource "aws_ecs_task_definition" "task" {
   "memory": 128,
   "essential": true,
   "portMappings": [
-    {
-      "containerPort": 3000,
-      "hostPort": 3000
-    }
+  {
+    "containerPort": 3000,
+    "hostPort": 3000
+  }
    ]
   }
 ]
@@ -29,4 +28,10 @@ resource "aws_ecs_service" "service" {
   cluster = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
   desired_count = 1
+  launch_type = "EC2"  # or "FARGATE"
+  network_configuration {
+    subnets = module.vpc.public_subnet_ids
+    security_groups = [aws_security_group.ecs_service.id]
+  }
+  depends_on = [aws_ecs_task_definition.task] 
 }
